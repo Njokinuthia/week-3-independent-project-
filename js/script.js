613f4c0946afe94c829fc0c931501895c2adead3
Njoki <kinuthiash@gmail.com>
2022-02-07 08:56:32 +0300

    feat:js -add feedback part

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
 
 // function to give scores to each question
-function grade ()
-{ 
-  var sum = 0; 
-  
-  if (document.getElementById("answer1").checked){
-      sum++;
+function grade() {
+  var sum = 0;
+
+  if (document.getElementById("answer1").checked) {
+    sum++;
   }
 
-  if (document.getElementById("answer2").checked){
+  if (document.getElementById("answer2").checked) {
     sum++;
   }
-  if (document.getElementById("answer3").checked){
+  if (document.getElementById("answer3").checked) {
     sum++;
   }
-  if (document.getElementById("answer4").checked){
+  if (document.getElementById("answer4").checked) {
     sum++;
   }
-  if (document.getElementById("answer5").checked){
+  if (document.getElementById("answer5").checked) {
     sum++;
   }
-  
+
   var answer = sum * 20;
 
-  document.write('Your score is' + ' ' + answer);
+  document.write('Your score is' + ' ' + answer + '%');
+  result(answer);
 }
 
 // code to get grade levels
-if (answer>=80){
-  document.write("excellent")
-}
-else if (answer<80 && answer >=50){
-  document.write(" You fairly passed")
-}
-else if (answer <50){
-  document.write("poorly performed")
+function result(answer) {
+
+  if (answer >= 80) {
+    document.write("excellent")
+  }
+  else if (answer < 80 && answer >= 50) {
+    document.write(" You fairly passed")
+  }
+  else if (answer < 50) {
+    document.write("poorly performed")
+  }
+
 }
